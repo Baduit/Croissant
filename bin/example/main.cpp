0a2c150bc4e6c95885a38d02eb083b7dd536f4eb
Baduit <simon.bertho@epitech.eu>
2020-12-09 22:46:13 +0100

    Harmonize the code between space and tab indentation

--- bin/example/main.cpp
+++ bin/example/main.cpp
@@ -9,14 +9,14 @@
 
 int main()
 {
-    using namespace Croissant;
-    using namespace Croissant::IntegerLiterals;
+	using namespace Croissant;
+	using namespace Croissant::IntegerLiterals;
 
-    Int a = 5;
-    auto b = 6_i;
+	Int a = 5;
+	auto b = 6_i;
 
-    if (a < b <= 6)
-        std::cout << "true" << std::endl;
-    else
-        std::cout << "false" << std::endl;
+	if (a < b <= 6)
+		std::cout << "true" << std::endl;
+	else
+		std::cout << "false" << std::endl;
 }
